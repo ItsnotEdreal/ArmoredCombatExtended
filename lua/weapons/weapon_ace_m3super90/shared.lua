59526683504b1477cfda93974b786c3a070c75e1
OrangeFox861 <rees08@bk.ru>
2026-01-09 06:34:29 +0500

    fix linter

--- lua/weapons/weapon_ace_m3super90/shared.lua
+++ lua/weapons/weapon_ace_m3super90/shared.lua
@@ -90,7 +90,7 @@ function SWEP:InitBulletData()
 	self.BulletData.ProjMass = self.BulletData.FrArea * (self.BulletData.ProjLength * 7.9 / 1000)
 	self.BulletData.PropMass = self.BulletData.FrArea * (self.BulletData.PropLength * ACF.PDensity / 1000) --Volume of the case as a cylinder * Powder density converted from g to kg
 	--		self.BulletData.DragCoef  = 0 --Alternatively manually set it
-			self.BulletData.DragCoef  = ((self.BulletData.FrArea/10000)/self.BulletData.ProjMass)
+	self.BulletData.DragCoef  = ((self.BulletData.FrArea / 10000) / self.BulletData.ProjMass)
 	--self.BulletData.DragCoef = 0.0075 --Alternatively manually set it
 	--Don't touch below here
 	self.BulletData.MuzzleVel = ACF_MuzzleVelocity(self.BulletData.PropMass, self.BulletData.ProjMass, self.BulletData.Caliber)
