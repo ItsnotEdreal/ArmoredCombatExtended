4d5f8b0eaa041976087c92f17487a071f8155b56
KemGus <dyer04@mail.ru>
2026-01-13 01:02:36 +0300

    forgot to add

--- lua/entities/ace_gforce_meter/init.lua
+++ lua/entities/ace_gforce_meter/init.lua
@@ -31,7 +31,7 @@ function ENT:Initialize()
 	self:UpdateOverlayText()
 end
 
-function MakeACE_GForce_Meter(Owner, Pos, Angle, Id, EntityData)
+function MakeACE_GForce_Meter(Owner, Pos, Angle, Id, _EntityData)
 	if not Owner:CheckLimit("_ace_gforce_meter") then return false end
 
 	Id = Id or "GForceMeter"
