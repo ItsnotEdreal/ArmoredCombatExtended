a8bb8f6b5785444a32549e9c1d8ff2a391ae1569
Ally-for-Hire <zacyandell@gmail.com>
2026-02-24 02:50:47 -0600

    Revert new console command names back to ace_ prefix

--- lua/acf/shared/sh_ace_functions.lua
+++ lua/acf/shared/sh_ace_functions.lua
@@ -375,7 +375,7 @@ do
 
 	if CLIENT then
 
-		concommand.Add( "acf_dupes_remount", function()
+		concommand.Add( "ace_dupes_remount", function()
 
 			if not AdvDupe2 then
 				notification.AddLegacy( "Unable to reload the dupes.", NOTIFY_ERROR, 7)
@@ -1710,6 +1710,7 @@ function ACE_GetArmorScan(ent)
 	local front, side = ACE_CalcContraptionArmor(ent)
 	return front, side
 end
+
 
 
 
